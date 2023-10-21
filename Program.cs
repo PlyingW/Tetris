a0b962905b277bed9a47842a426d74dbd90b946a
PlyingW <alanom@hotmail.es>
2023-10-21 16:44:41 -0600

    Tetris Actualizado
    Hacer la pieza girar

--- Program.cs
+++ Program.cs
@@ -3,63 +3,178 @@ using System.Threading;
 
 class Program
 {
+    static int screenWidth = 20;
+    static int screenHeight = 20;
+    static char border = '*';
+    static char block = '#';
+    static char empty = ' ';
+    static Random random = new Random();
+    static int currentX = 8;
+    static int currentY = 0;
+    static int blockWidth = 4;
+    static int blockHeight = 2;
+    static char[,] playfield = new char[screenHeight, screenWidth];
+
+    
+    static char[,] figure1 = new char[,] {
+        { block, block },
+        { block, block },
+    };
+
+    static char[,] figure2 = new char[,] {
+        { block, block, block, block }
+    };
+
+   
+    static bool useFigure1 = true;
+
     static void Main()
     {
-        int windowHeight = 20;  
-        int windowWidth = 40;   
+        Console.CursorVisible = false;
+        Console.WindowHeight = screenHeight + 1;
+        Console.WindowWidth = screenWidth + 1;
 
-        
-        Console.WindowHeight = windowHeight;
-        Console.WindowWidth = windowWidth;
-        Console.BufferHeight = windowHeight;
-        Console.BufferWidth = windowWidth;
-
-        int squareSize = 5;     
-        int squareX = windowWidth / 2 - squareSize / 2; 
-        int squareY = 0;         
-        int groundY = windowHeight - 1;
+        InitializePlayfield();
+        CreateNewBlock();
 
         while (true)
         {
-         
-            DrawSquare(squareX, squareY, squareSize);
+            Console.Clear();
+            DrawBorder();
+            DrawPlayfield();
+            DrawBlock(currentX, currentY);
 
-            
-            squareY++;
-
-            
-            if (squareY + squareSize >= groundY)
+            if (currentY + blockHeight < screenHeight && !CheckCollision(0, 1))
             {
-                ClearSquare(squareX, squareY - 1, squareSize);
+                currentY++;
+            }
+            else
+            {
+                
+                LockBlock();
+                CreateNewBlock();
             }
 
-           
-            Thread.Sleep(100);
+            if (Console.KeyAvailable)
+            {
+                var key = Console.ReadKey(true).Key;
+                if (key == ConsoleKey.Spacebar)
+                {
+                    
+                    useFigure1 = !useFigure1;
+                }
+                else
+                {
+                    MoveBlock(key);
+                }
+            }
+
+            Thread.Sleep(200);
         }
     }
 
- 
-    static void DrawSquare(int x, int y, int size)
+    static void InitializePlayfield()
     {
-        for (int i = y; i < y + size; i++)
+        for (int i = 0; i < screenHeight; i++)
         {
-            Console.SetCursorPosition(x, i);
-            for (int j = x; j < x + size; j++)
+            for (int j = 0; j < screenWidth; j++)
             {
-                Console.Write("■");
+                playfield[i, j] = empty;
             }
         }
     }
 
-    
-    static void ClearSquare(int x, int y, int size)
+    static void DrawBorder()
     {
-        for (int i = y; i < y + size; i++)
+        for (int i = 0; i < screenHeight; i++)
         {
-            Console.SetCursorPosition(x, i);
-            for (int j = x; j < x + size; j++)
+            Console.SetCursorPosition(0, i);
+            Console.Write(border);
+            Console.SetCursorPosition(screenWidth, i);
+            Console.Write(border);
+        }
+        for (int i = 0; i < screenWidth; i++)
+        {
+            Console.SetCursorPosition(i, screenHeight);
+            Console.Write(border);
+        }
+    }
+
+    static void DrawPlayfield()
+    {
+        for (int i = 0; i < screenHeight; i++)
+        {
+            for (int j = 0; j < screenWidth; j++)
             {
-                Console.Write(" ");
+                Console.SetCursorPosition(j, i);
+                Console.Write(playfield[i, j]);
+            }
+        }
+    }
+
+    static void DrawBlock(int x, int y)
+    {
+        char[,] currentFigure = useFigure1 ? figure1 : figure2;
+
+        for (int i = 0; i < currentFigure.GetLength(0); i++)
+        {
+            for (int j = 0; j < currentFigure.GetLength(1); j++)
+            {
+                Console.SetCursorPosition(x + j, y + i);
+                if (currentFigure[i, j] == block)
+                {
+                    Console.Write(block);
+                }
+            }
+        }
+    }
+
+    static void CreateNewBlock()
+    {
+        currentX = 8;
+        currentY = 0;
+        blockWidth = useFigure1 ? 2 : 4;
+        blockHeight = useFigure1 ? 2 : 1;
+        useFigure1 = !useFigure1;
+    }
+
+    static void MoveBlock(ConsoleKey key)
+    {
+        if (key == ConsoleKey.A && currentX > 0 && !CheckCollision(-1, 0))
+        {
+            currentX--;
+        }
+        else if (key == ConsoleKey.D && currentX + blockWidth < screenWidth && !CheckCollision(1, 0))
+        {
+            currentX++;
+        }
+    }
+
+    static bool CheckCollision(int offsetX, int offsetY)
+    {
+        for (int i = 0; i < blockHeight; i++)
+        {
+            for (int j = 0; j < blockWidth; j++)
+            {
+                if (currentY + i + offsetY >= screenHeight || currentX + j + offsetX >= screenWidth || currentX + j + offsetX < 0 || playfield[currentY + i + offsetY, currentX + j + offsetX] == block)
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    static void LockBlock()
+    {
+        for (int i = 0; i < blockHeight; i++)
+        {
+            for (int j = 0; j < blockWidth; j++)
+            {
+                if (currentY + i < screenHeight)
+                {
+                    playfield[currentY + i, currentX + j] = block;
+                }
             }
         }
     }
